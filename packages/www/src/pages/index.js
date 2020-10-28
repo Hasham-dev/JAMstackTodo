c48f99481cbf4998d0accd581a3de22da99e8c38
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-10-29 00:14:09 +0500

    Update

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {Button, Container,Flex,Heading} from "theme-ui";
 
 export default props =>(
-    <div>
-        Hello World
-    </div>
+    <Container>
+        <Flex sx={{flexDirection: "column",padding: 3}}>
+
+        <Heading as="h1">Todo App</Heading>
+        <Button sx={{marginTop: 2}} onClick={()=>alert("Login")}>Login</Button>
+        </Flex>
+    </Container>
 );
