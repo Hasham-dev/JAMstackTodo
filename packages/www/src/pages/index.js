2a2cc46555eee0e05955893c656501b37d74ff90
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-10-29 00:15:33 +0500

    Database Gatsby & Theme UI

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -6,7 +6,7 @@ export default props =>(
         <Flex sx={{flexDirection: "column",padding: 3}}>
 
         <Heading as="h1">Todo App</Heading>
-        <Button sx={{marginTop: 2}} onClick={()=>alert("Login")}>Login</Button>
+        <Button sx={{marginTop: 2,color: "black"}} onClick={()=>alert("Login")}>Login</Button>
         </Flex>
     </Container>
 );
