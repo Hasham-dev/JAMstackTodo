dfb6398b2334d7939a3d712da30ff69cdee6798f
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-10-30 01:11:59 +0500

    Mid level Completing
    Adding app.js & Render Just UserName in it

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,18 +1,45 @@
-import React, { useEffect } from 'react';
-import { Button, Container, Flex, Heading } from "theme-ui";
+import React, { useEffect, useState } from 'react';
+import { Button, Container, Flex, Heading, NavLink } from "theme-ui";
 import netlifyIdentity from 'netlify-identity-widget'
+import { Link } from 'gatsby';
+
+
 export default props => {
+    const [user, setUser] = useState();
+
+
     useEffect(() => {
         netlifyIdentity.init({});
+    })
+    netlifyIdentity.on("login", user => {
+        netlifyIdentity.close();
+        setUser(user);
+    })
+
+    netlifyIdentity.on("logout", () => {
+        netlifyIdentity.close();
+        setUser();
     })
 
     return (
 
         <Container>
+            <Flex as='nav'>
+                <NavLink as={Link} to="/" p={2}>
+                    Home
+                </NavLink>
+                <NavLink as={Link} to={"/app"} p={2}>
+                    Dashboard
+                </NavLink>
+                {user && (<NavLink p={2}>
+                   Hello {user.user_metadata.full_name} !
+                </NavLink>)}
+
+            </Flex>
             <Flex sx={{ flexDirection: "column", padding: 3 }}>
 
                 <Heading as="h1">Todo App</Heading>
-                <Button sx={{ marginTop: 2, color: "black" }} onClick={() => {netlifyIdentity.open()}}>Login</Button>
+                <Button sx={{ marginTop: 2, color: "black" }} onClick={() => { netlifyIdentity.open() }}>Login</Button>
             </Flex>
         </Container>
     )
